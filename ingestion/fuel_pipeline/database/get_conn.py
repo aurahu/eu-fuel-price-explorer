2596fd4aa2c0c252b8256c3fce2b8a436a20e055
auroora <135814836+auroora@users.noreply.github.com>
2026-09-21 18:47:02 +0300

    add timeout for connecting

--- ingestion/fuel_pipeline/database/get_conn.py
+++ ingestion/fuel_pipeline/database/get_conn.py
@@ -10,5 +10,6 @@ def get_connection():
         dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        sslmode="require"
+        sslmode="require",
+        connect_timeout=10,
     )
